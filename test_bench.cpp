04b0030717b579f9bcdf59e1b2bcb17d26a4f453
Jerome Maloberti <jerome.maloberti@neubla.com>
2025-02-09 08:15:55 +0000

    Add aligned dot.

--- test_bench.cpp
+++ test_bench.cpp
@@ -4,9 +4,10 @@
 #include <eve/module/algo.hpp>
 #include <functional>
 #include <nanobench.h>
+#include <span>
 #include <vector>
 
-constexpr float dot(const std::vector<float> &l, const std::vector<float> &r) {
+constexpr float dot(const std::span<float> &l, const std::span<float> &r) {
   float cum = 0.0;
   for (size_t i = 0; i < l.size(); ++i) {
     cum += l[i] * r[i];
@@ -14,20 +15,27 @@ constexpr float dot(const std::vector<float> &l, const std::vector<float> &r) {
   return cum;
 }
 
-float dot_eve(const std::vector<float> &l, const std::vector<float> &r) {
+float dot_eve(const std::span<float> l, const std::span<float> r) {
   std::vector<float> out(l.size());
   eve::algo::transform_to(eve::views::zip(l, r), out,
                           [](auto xy) { return get<0>(xy) * get<1>(xy); });
   return eve::algo::reduce(out, 0.0);
 }
 
-float dot_eve2(const std::vector<float> &l, const std::vector<float> &r) {
+float dot_eve2(const std::span<float> l, const std::span<float> r) {
   return eve::algo::transform_reduce(
       eve::views::zip(l, r), [](auto xy) { return get<0>(xy) * get<1>(xy); },
       0.0);
 }
 
-float dot_eve3(const std::vector<float> &l, const std::vector<float> &r) {
+float dot_eve3(const std::span<float> l, const std::span<float> r) {
+  return eve::algo::transform_reduce[eve::algo::fuse_operations](
+      eve::views::zip(l, r),
+      [](auto xy, auto sum) { return eve::fma(get<0>(xy), get<1>(xy), sum); },
+      0.0f);
+}
+
+float dot_eve_aligned(const std::span<float> l, const std::span<float> r) {
   return eve::algo::transform_reduce[eve::algo::fuse_operations](
       eve::views::zip(l, r),
       [](auto xy, auto sum) { return eve::fma(get<0>(xy), get<1>(xy), sum); },
@@ -36,7 +44,7 @@ float dot_eve3(const std::vector<float> &l, const std::vector<float> &r) {
 
 void dot_bench(
     const std::string &name,
-    std::function<float(const std::vector<float> &, const std::vector<float> &)>
+    std::function<float(const std::span<float>, const std::span<float>)>
         f) {
   ankerl::nanobench::Bench benchmark;
   benchmark.title(name).performanceCounters(true).minEpochTime(
@@ -71,4 +79,23 @@ TEST_CASE("dot_bench", "[benchmark]") {
   dot_bench("dot_eve", dot_eve);
   dot_bench("dot_eve2", dot_eve2);
   dot_bench("dot_eve3", dot_eve3);
+
+  ankerl::nanobench::Bench benchmark;
+  benchmark.title("dot_eve_aligned")
+      .performanceCounters(true)
+      .minEpochTime(std::chrono::milliseconds(100));
+
+  for (size_t length = 10; length <= 100'000; length *= 10) {
+    alignas(eve::wide<float>::alignment()) float in1[length];
+    alignas(eve::wide<float>::alignment()) float in2[length];
+    for (size_t i = 0; i < length; ++i) {
+      in1[i] = 2.4;
+      in2[i] = 4.2;
+    }
+    benchmark.run(std::to_string(length), [&] {
+      float res = dot_eve_aligned({in1, length}, {in2, length});
+      ankerl::nanobench::doNotOptimizeAway(res);
+      return res;
+    });
+  }
 }
